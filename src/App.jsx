dd871b9bcbe2bdbd10e01ac0589c69b569c7368f
Anitha-Arjun <arjunmurugan@Arjuns-MacBook-Air.local>
2024-10-16 15:46:21 -0500

    CRUD Completed:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ProjectsPage from "./pages/ProjectsPage";
+import NavBar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProjectDetails from "./pages/ProjectDetails";
 import "./App.css";
 
 function App() {
   const [projects, setProjects] = useState(null);
-  const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-  });
+
   //fetch all project when component first render
   useEffect(() => {
     //fetch all projects
@@ -19,70 +21,25 @@ function App() {
     fetchProjects();
   }, []);
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      //make POST request to create a new project
-      const res = await fetch("http://localhost:4000/api/projects/", {
-        headers: {
-          "Content-Type": "applicaiton/json",
-        },
-        method: "POST",
-        body: JSON.stringify(formData),
-      });
-      //parse data to json
-      const data = await res.json();
-      console.log(data.project);
-
-      //adding the new created project to the projects array
-      setProjects([data.project, ...projects]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
-      <h1>Project Manager</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">
-            Project Name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="description">
-            Project Description:
-            <input
-              type="text"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-            />
-          </label>
-          <input type="submit" value="create project" />
-        </form>
-      </div>
-      <div>
-        {projects &&
-          projects.map((project) => (
-            <div key={project._id}>
-              <h2>{project.name}</h2>
-              <p>{project.description}</p>
-            </div>
-          ))}
-      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route
+          path="/projects"
+          element={
+            <ProjectsPage projects={projects} setProjects={setProjects} />
+          }
+        />
+
+        <Route
+          path="/projects/:id"
+          element={
+            <ProjectDetails projects={projects} setProjects={setProjects} />
+          }
+        />
+      </Routes>
     </>
   );
 }
